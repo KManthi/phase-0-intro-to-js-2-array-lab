636f81745177407e6c87cafec7323b71acb5e85d
Daniel <dani3lkamanthi@gmail.com>
2024-03-14 11:53:39 +0300

    Modifies files

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Leo) {
+    cats.push(Leo);
+}
+
+function destructivelyPrependCat(Chloe) {
+    cats.unshift(Chloe);
+}
+
+function destructivelyRemoveLastCat(Garfield) {
+    cats.pop(Garfield);
+}
+
+function destructivelyRemoveFirstCat(Milo) {
+    cats.shift(Milo);
+}
+
+function appendCat(Cuttie) {
+   return[...cats, Cuttie];
+}
+
+function prependCat(Minksy) {
+    return [Minksy, ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
